7c54e9743d519f9e16308d153015355c846d811d
Thomas Cyron <thomas@thcyron.de>
2015-12-30 14:49:20 +0100

    Merge document.go and gpx.go

--- gpx.go
+++ gpx.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"strconv"
+	"time"
 )
 
 const nsGPX11 = "http://www.topografix.com/GPX/1/1"
@@ -14,6 +15,39 @@ var (
 	ErrBadRootTag = errors.New("gpx: root element must be <gpx>")
 	ErrGPX11Only  = errors.New("gpx: can only parse GPX 1.1 documents")
 )
+
+// Document represents a GPX document.
+type Document struct {
+	Version  string
+	Metadata Metadata
+	Tracks   []Track
+}
+
+// Metadata provides additional information about a GPX document.
+type Metadata struct {
+	Time time.Time
+}
+
+// Track represents a track.
+type Track struct {
+	Segments []Segment
+}
+
+// Segments represents a track segment.
+type Segment struct {
+	Points []Point
+}
+
+// Point represents a track point. Extensions contains the raw XML tokens
+// of the point’s extensions if it has any (excluding the <extensions>
+// start and end tag).
+type Point struct {
+	Latitude   float64
+	Longitude  float64
+	Elevation  float64
+	Time       time.Time
+	Extensions []xml.Token
+}
 
 // Decoder decodes a GPX document from an input stream.
 type Decoder struct {
