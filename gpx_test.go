8e1a48085dc53c48ba8c940b28eb139f6c6cdc89
Thomas Cyron <thomas@thcyron.de>
2016-02-10 22:44:34 +0100

    Parse <metadata> completely

--- gpx_test.go
+++ gpx_test.go
@@ -39,11 +39,6 @@ func TestDecoder(t *testing.T) {
 		t.Errorf("got %v end; expected %v", end, expectedEnd)
 	}
 
-	metadataTime := time.Date(2015, 12, 13, 18, 35, 18, 0, time.UTC)
-	if !doc.Metadata.Time.Equal(metadataTime) {
-		t.Errorf("got %v, expected %v", doc.Metadata.Time, metadataTime)
-	}
-
 	if l := len(doc.Tracks); l != 1 {
 		t.Errorf("got %d track(s); expected 1", l)
 	}
@@ -97,6 +92,84 @@ func TestDecoder(t *testing.T) {
 		if !testCase.point.Time.Equal(testCase.t) {
 			t.Errorf("point test case %d: got %v time; expected %v", i, testCase.point.Time, testCase.t)
 		}
+	}
+
+	testMetadata(t, doc.Metadata)
+}
+
+func testMetadata(t *testing.T, metadata Metadata) {
+	if expected := "Run"; metadata.Name != expected {
+		t.Errorf("expected name %q; got %q", expected, metadata.Name)
+	}
+	if expected := "Running in the forest"; metadata.Description != expected {
+		t.Errorf("expected description %q; got %q", expected, metadata.Description)
+	}
+	if expected := "run running forest sport sports"; metadata.Keywords != expected {
+		t.Errorf("expected keywords %q; got %q", expected, metadata.Keywords)
+	}
+	if expected := time.Date(2015, 12, 13, 18, 35, 18, 0, time.UTC); !metadata.Time.Equal(expected) {
+		t.Errorf("expected time %q; got %q", expected, metadata.Time)
+	}
+
+	testMetadataLink(t, metadata.Link)
+	testMetadataCopyright(t, metadata.Copyright)
+	testMetadataBounds(t, metadata.Bounds)
+	testMetadataAuthor(t, metadata.Author)
+}
+
+func testMetadataLink(t *testing.T, link Link) {
+	if expected := "http://www.runtastic.com"; link.Href != expected {
+		t.Errorf("expected link href %q; got %q", expected, link.Href)
+	}
+	if expected := "runtastic"; link.Text != expected {
+		t.Errorf("expected link text %q; got %q", expected, link.Text)
+	}
+	if expected := "text/html"; link.Type != expected {
+		t.Errorf("expected link type %q; got %q", expected, link.Type)
+	}
+}
+
+func testMetadataCopyright(t *testing.T, copyright Copyright) {
+	if expected := "www.runtastic.com"; copyright.Author != expected {
+		t.Errorf("expected copyright author %q; got %q", expected, copyright.Author)
+	}
+	if expected := 2015; copyright.Year != expected {
+		t.Errorf("expected copyright year %d; got %d", expected, copyright.Year)
+	}
+	if expected := "http://www.runtastic.com"; copyright.License != expected {
+		t.Errorf("expected copyright license %q; got %q", expected, copyright.License)
+	}
+}
+
+func testMetadataBounds(t *testing.T, bounds Bounds) {
+	if expected := 49.3956527709960938; bounds.MinLatitude != expected {
+		t.Errorf("expected bounds min latitude %f; got %f", expected, bounds.MinLatitude)
+	}
+	if expected := 49.4017448425292969; bounds.MaxLatitude != expected {
+		t.Errorf("expected bounds max latitude %f; got %f", expected, bounds.MaxLatitude)
+	}
+	if expected := 11.1253080368041992; bounds.MinLongitude != expected {
+		t.Errorf("expected bounds min longitude %f; got %f", expected, bounds.MinLongitude)
+	}
+	if expected := 11.1280641555786133; bounds.MaxLongitude != expected {
+		t.Errorf("expected bounds max longitude %f; got %f", expected, bounds.MaxLongitude)
+	}
+}
+
+func testMetadataAuthor(t *testing.T, person Person) {
+	if expected := "Runtastic"; person.Name != expected {
+		t.Errorf("expected person name %q; got %q", expected, person.Name)
+	}
+
+	testMetadataAuthorEmail(t, person.Email)
+}
+
+func testMetadataAuthorEmail(t *testing.T, email Email) {
+	if expected := "runtastic"; email.ID != expected {
+		t.Errorf("expected email ID %q; got %q", expected, email.ID)
+	}
+	if expected := "example.com"; email.Domain != expected {
+		t.Errorf("expected email domain %q; got %q", expected, email.Domain)
 	}
 }
 
